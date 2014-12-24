9efdb079ab5b26acf5a2fed8d0a27d0233eb8d2c
unknown <acer@acer-PC.(none)>
2014-12-24 22:32:53 +0530

    First commit

---
+++ src/main/java/foodjunction/FoodjunctionApplication.java
@@ -0,0 +1,16 @@
+package foodjunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class FoodjunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodjunctionApplication.class, args);
+    }
+}
